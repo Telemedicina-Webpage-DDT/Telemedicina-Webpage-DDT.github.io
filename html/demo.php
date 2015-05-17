3c593d8bdb524aa70c92bcc8591c1176d4c6c18b
Danilo Diaz Tarasco <danilodt@gmail.com>
2015-05-17 16:52:39 -0430

    Prueba de Login y Cambio de header

---
+++ html/demo.php
@@ -0,0 +1,76 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    
+
+    <!-- Custom styles for this template -->
+    <link href="signin.css" rel="stylesheet">
+      
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
+
+    <!-- Custom Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="../css/animate.min.css" type="text/css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="../css/creative.css" type="text/css">
+      
+      
+      
+    <title>Acceda a Telemedicina</title>
+
+
+  </head>
+
+  <body>
+      
+  <header>
+        <div class="header-content">
+            
+
+    <div class="container">
+    <div class="row">
+      <form class="col-lg-12 text-center">
+        <h2 class="form-signin-heading">Acceda</h2>
+      </form>
+    </div>
+    
+    <div class="row">
+    <div class="col-lg-4 text-center"></div>
+    <form class="col-lg-4 text-center">
+        <label for="inputEmail" class="sr-only">Dirección de Email</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Dirección de Email" required autofocus>
+        <label for="inputPassword" class="sr-only">Contraseña</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Recordar Contraseña
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Ingresa</button>
+      </form>
+    </div>
+    </div> <!-- /container -->
+      </div>
+      </header>
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
